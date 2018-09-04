1146919fbdc03a652510097e62b5ce8d8b112458
tharindutcj <tharindutcj@gmail.com>
2018-09-04 22:27:41 +0530

    Supervisor Login implemented

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const formsRouter = require('./routes/forms');
+const studentRouter = require('./routes/student');
+const supervisorRouter = require('./routes/supervisor');
+const cors = require('cors');
 
 const app = express();
 
@@ -13,10 +16,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/forms', formsRouter);
+app.use('/student', studentRouter);
+app.use('/supervisor', supervisorRouter);
 
 module.exports = app;
