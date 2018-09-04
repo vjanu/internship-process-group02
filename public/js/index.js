0bbadbab35dc2bf995b26ddc689acfc4d4d32d67
Anushka Yohan <anushka.yohan@pearson.com>
2018-09-04 21:29:03 +0530

    Modify index.js in frontend to parse the form-i-1 url tagged with student id.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 /* * * * *     Global Variables     * * * * */
-let baseUrlLocal = 'http://localhost:3000';
+let baseUrl = 'http://localhost:3000';
 let baseUrlProd = 'http://ec2-18-209-163-192.compute-1.amazonaws.com:3000';
-let baseUrl = baseUrlLocal;
 
 /* * * * *     Event Triggers     * * * * */
 // form submit for form I-1, student perspective.
@@ -12,7 +11,16 @@ $('#btn-form-i-1-student').on('click', function () {
 $('#btn-form-i-1-supervisor').on('click', function () {
     getFormI1SupervisorDetails();
 });
+// check if the loaded page is a form-i-1 page with a student id embeded in the url.
+let current_url = window.location.href;
+if (current_url.includes('#') && current_url.includes('form-i-1')) {
+    studentId = current_url.substr(current_url.indexOf('#')+1, current_url.length);
+    populateFormI1(studentId);
+}
 
+
+
+/* * * * * * * Forms * * * * * * */
 
 /*
  * this will get the information filled by the student,
@@ -89,3 +97,102 @@ function getFormI1SupervisorDetails() {
         })
 }
 
+/*
+ * For the supervisor to enter details, we need to show the Form I-1 of a specific student.
+ * For this, we append #<studentId> to the URL of the form-i-1.html and let the following function
+ * get data of the above student and show on the student section of the form.
+ */
+function populateFormI1(studentId) {
+    axios.get(baseUrl + '/forms/form-i-1/student/' + studentId)
+        .then(response => {
+            if (response.data.success) {
+                let form_details = response.data.data;
+                console.log(form_details);
+
+                document.getElementById('name-student').value = form_details['StudentName'];
+                document.getElementById('id-student').value = form_details['StudentId'];
+                document.getElementById('address-student').value = form_details['StudentAddress'];
+                document.getElementById('home-phone-student').value = form_details['StudentHomePhone'];
+                document.getElementById('mobile-phone-student').value = form_details['StudentMobilePhone'];
+                document.getElementById('cgpa-student').value = form_details['CGPA'];
+                document.getElementById('emails-student').value = form_details['StudentEmails'].join(', ').replace('[').replace(']');
+                document.getElementById('year-student').value = form_details['Year'];
+                document.getElementById('semester-student').value = form_details['Semester'];
+
+                // iterate through each input element and feed the above data, but keep the text boxes disabled.let elems = $('#form-i-1-student').find(':input');
+                let elems = $('#form-i-1-student').find(':input');
+                for (let i = 0; i < elems.length; i++) {
+                    elems[i].innerHTML
+                    elems[i].disabled = true;
+                }
+            }
+        })
+        .catch(reject => {
+            console.log(reject);
+        })
+}
+
+
+/* * * * * * * Dashboards * * * * * * */
+
+function getAllInternships() {
+    let headerMap = new Map();
+
+    // setting headers and their corresponding json keys.
+    headerMap.set('Student ID', 'StudentId');
+    headerMap.set('Student Name', 'StudentName');
+    headerMap.set('Internship Start', '-');
+    headerMap.set('Internship End', '-');
+    headerMap.set('Forms', '-');
+
+    axios.get(baseUrl + '/forms/form-i-1')
+        .then(response => {
+            if (response.data.success) {
+                console.log(response.data.data);
+                let table = renderInternshipsTable(headerMap, response.data.data);
+                $('#internships-table').append(table);
+            }
+        })
+}
+
+/*
+ * this will render the internships manager's dashboard.
+ * 
+ * @param columnMap:
+ *      a map where the key is the column name and the value is the key of the json data,
+ *      that is relevant to the column.
+ *      
+ */
+function renderInternshipsTable(columnMap, jsonData) {
+    console.log(columnMap);
+
+    // create table and headers.
+    let table = document.createElement('table');
+    table.classList = 'table';
+
+    let thead = document.createElement('thead');
+    let headerTr = document.createElement('tr');
+
+    for (let [key, value] of columnMap.entries()) {
+        let th = document.createElement('th');
+        th.setAttribute('scope', 'col');
+        th.innerHTML = key;
+        headerTr.appendChild(th);
+    }
+    thead.appendChild(headerTr);
+
+    // create the body rows.
+    let tbody = document.createElement('tbody');
+    jsonData.forEach(entry => {
+        let tr = document.createElement('tr');
+
+        for (let [key, value] of columnMap.entries()) {
+            let td = document.createElement('td');
+            td.innerHTML = entry[value];
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    });
+    table.append(thead, tbody);
+    return table;
+}
