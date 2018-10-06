9025ce0452c770580064627e88633d65f851fa34
Anushka Yohan <aliyanage44@gmail.com>
2018-10-06 15:19:06 +0530

    Add db model for Viva schedule.

---
+++ models/scheduleModel.js
@@ -0,0 +1,15 @@
+const db = require('./mongodb').db;
+const mongoose = require('./mongodb').mongoose;   // connection is already established @mongodb.js file.
+
+let scheduleSchema = mongoose.Schema({
+    // following are filled by student.
+    StudentId: String,
+    VivaDate: Date,
+    Location: String
+});
+
+
+
+let scheduleModel = mongoose.model('schedule', scheduleSchema);
+
+module.exports = { scheduleModel };
